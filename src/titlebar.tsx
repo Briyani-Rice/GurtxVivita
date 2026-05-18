728d0327c096f7d04528064d9fda4f56a89cce15
ezekielongkaibin <ezekiel_ong_kai_bin@s2025.ssts.edu.sg>
2026-05-18 16:18:41 +0800

    test

--- src/titlebar.tsx
+++ src/titlebar.tsx
@@ -23,7 +23,11 @@ export default function Titlebar({ tabs,setTabIndex, handleNewTab, setTab }: Pro
     return (<div className="title">
         <h3
             className="title-text"
-            style={{ paddingLeft: `${leftPadding}px`}}
+            style={{
+                 paddingLeft: `${leftPadding}px`,
+                 fontSize:`15px`
+                }}
+            
         >
             Viventory
         </h3>
